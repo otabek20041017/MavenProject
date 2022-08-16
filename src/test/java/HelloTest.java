ebecff03a6d26d7f4bde860b90ee53c99e139f56
Otabek Yusupov <news@email>
2022-08-16 16:44:08 +0500

    started tracking .idea folder

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,10 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("HelloTest");
 
+        // work 1
+
+        // work 2
+
+        // work 3
     }
 }
