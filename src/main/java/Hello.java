70388d3794e0411e062266e961f142d6aee08fba
Otabek Yusupov <news@email>
2022-08-16 21:24:12 +0500

    add more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello features");
+        System.out.println("Local Repo is already published to Git Hub");
+        System.out.println("Doing more work");
     }
+
 }
